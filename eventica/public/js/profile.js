f338e5bc0450db1802141237d1e2e302d15ca5a6
bhanuc <bhanuc@iitk.ac.in>
2014-11-10 13:09:30 +0530

    done full thing

--- eventica/public/js/profile.js
+++ eventica/public/js/profile.js
@@ -14,22 +14,43 @@ var getprofile = function getprofile () {
             console.log(profile[i])
             switch (profile[i]) {
                 case "alternatenumber":
-            string = string+'<div class="row"><div class="col-md-6">Alternate person of contact</div>'+'<div class="col-md-6">not written</div>'+'</div>';
+            string = string+'<div class="row profile"><div class="col-md-6">Alternate person of contact</div>'+'<div class="col-md-6">not written</div>'+'</div>';
                break;
                 case "apnr":
-                string = string+'<div class="row"><div class="col-md-6">Arrival PNR</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+                string = string+'<div class="row  profile"><div class="col-md-6">Arrival PNR</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
                break;
                 case "adate":
-            string = string+'<div class="row"><div class="col-md-6">Arrival Date</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+            string = string+'<div class="row profile"><div class="col-md-6">Arrival Date</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
                break;
                 case "dpnr":
-            string = string+'<div class="row"><div class="col-md-6">Departure PNR</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+            string = string+'<div class="row profile"><div class="col-md-6">Departure PNR</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
                break;
                 case "ddate":
-            string = string+'<div class="row"><div class="col-md-6">Departure Date</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+            string = string+'<div class="row profile"><div class="col-md-6">Departure Date</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+               break;
+               case "name":
+            string = string+'<div class="row profile"><div class="col-md-6">Name</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+               break;
+               case "number":
+            string = string+'<div class="row profile"><div class="col-md-6">Mobile No.</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+               break;
+               case "college":
+            string = string+'<div class="row profile"><div class="col-md-6">College Name</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+               break;
+               case "ambassador":
+            string = string+'<div class="row profile"><div class="col-md-6">Ambassador</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+               break;
+               case "email":
+            string = string+'<div class="row profile"><div class="col-md-6">Email</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+               break;
+               case "branch":
+            string = string+'<div class="row profile"><div class="col-md-6">Branch</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+               break;
+               case "sex":
+            string = string+'<div class="row profile"><div class="col-md-6">Sex</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
                break;
               default:
-                    string = string+'<div class="row"><div class="col-md-6">'+profile[i]+'</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
+                    string = string+'<div class="row profile"><div class="col-md-6">'+profile[i]+'</div><div class="col-md-6">'+(data.profile[profile[i]]|| ' ')+'</div></div>';
               }
             };
 	
@@ -48,22 +69,54 @@ $('#EditButton').click(function editbutton () {
     if(global && global.profile){
       var form = '<form>';
       for (var i = 0; i <= global.keys.length - 1; i++) {
-	if(global.keys[i] == 'id'){
-	continue;
-	}
-  if(global.keys[i] == 'name' || global.keys[i] == 'email') {
-      form = form+'<div class="row"><div class="col-md-6">'+global.keys[i]+'</div><div class="col-md-6"><input type="text" value="'+(global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm" disabled ></div></div>';
-            } else if(global.keys[i] == 'alternatenumber'){
-            if(global.profile[global.keys[i]]){
-           var input =  global.profile[global.keys[i]].split('$$');
-                    form = form+'<div class="row"><div class="col-md-6"> Alternate person of contact: </div><div class="col-md-6"><input type="text" value="'+input[0]+'" id="alternatenumber1" name="profileForm"  >  <input type="text" value="'+(input[1] || 'Not submitted')+'" id="alternatenumber2" name="profileForm"  > <input type="text" value="'+(input[2] || "Not submitted")+'" id="alternatenumber3" name="profileForm"  ></div></div>';
-            } else {
-                    form = form+'<div class="row"><div class="col-md-6">Alternate person of contact:</div><div class="col-md-6"><input type="text" value="Name" id="alternatenumber1" name="profileForm"  >  <input type="text" value="Email" id="alternatenumber2" name="profileForm"  ><input type="text" value="Contact No" id="alternatenumber3" name="profileForm"  ></div></div>';   
-                  } 
-                }  else {
-              form = form+'<div class="row"><div class="col-md-6">'+global.keys[i]+'</div><div class="col-md-6"><input type="text" value="'+( global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
-              } 
-           }
+        
+        switch (global.keys[i]) {
+                case "id":
+                break;
+                case "alternatenumber":
+                  if(global.profile[global.keys[i]]){
+                      var input =  global.profile[global.keys[i]].split('$$');
+                          form = form+'<div class="row profile"><div class="col-md-6"> Alternate person of contact: </div><div class="col-md-6"><input type="text" value="'+input[0]+'" id="alternatenumber1" name="profileForm"  >  <input type="text" value="'+(input[1] || 'Not submitted')+'" id="alternatenumber2" name="profileForm"  > <input type="text" value="'+(input[2] || "Not submitted")+'" id="alternatenumber3" name="profileForm"  ></div></div>';
+                          } else {
+                              form = form+'<div class="row profile"><div class="col-md-6">Alternate person of contact:</div><div class="col-md-6"><input type="text" value="Name" id="alternatenumber1" name="profileForm"  >  <input type="text" value="Email" id="alternatenumber2" name="profileForm"  ><input type="text" value="Contact No" id="alternatenumber3" name="profileForm"  ></div></div>';   
+                            }
+                break;
+                case "apnr":
+              form = form+'<div class="row profile"><div class="col-md-6">Arrival PNR</div><div class="col-md-6"><input type="text" value="'+( global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
+               break;
+                case "adate":
+              form = form+'<div class="row profile"><div class="col-md-6">Arrival Date</div><div class="col-md-6"><input type="text" value="'+( global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
+               break;
+                case "dpnr":
+              form = form+'<div class="row profile"><div class="col-md-6">Departure PNR</div><div class="col-md-6"><input type="text" value="'+( global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
+               break;
+                case "ddate":
+              form = form+'<div class="row profile"><div class="col-md-6">Departure Date</div><div class="col-md-6"><input type="text" value="'+( global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
+               break;
+               case "name":
+              form = form+'<div class="row profile"><div class="col-md-6">Name</div><div class="col-md-6"><input type="text" value="'+(global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm" disabled ></div></div>';
+               break;
+               case "email":
+              form = form+'<div class="row profile"><div class="col-md-6">Email</div><div class="col-md-6"><input type="text" value="'+(global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm" disabled ></div></div>';
+               break;
+               case "number":
+              form = form+'<div class="row profile"><div class="col-md-6">Number</div><div class="col-md-6"><input type="text" value="'+( global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
+               break;
+               case "college":
+              form = form+'<div class="row profile"><div class="col-md-6">College</div><div class="col-md-6"><input type="text" value="'+( global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
+               break;
+               case "ambassador":
+              form = form+'<div class="row profile"><div class="col-md-6">Are you a Ambassador</div><div class="col-md-6"><input type="checkbox" value="'+( global.profile[global.keys[i]] || '')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
+               break;
+               case "branch":
+              form = form+'<div class="row profile"><div class="col-md-6">Enter your Branch</div><div class="col-md-6"><input type="text" value="'+( global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
+               break;
+               case "sex":
+              form = form+'<div class="row profile"><div class="col-md-6">Enter your Gender</div><div class="col-md-6"><input type="text" value="'+( global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
+               break;
+              default:
+              form = form+'<div class="row profile"><div class="col-md-6">'+global.keys[i]+'</div><div class="col-md-6"><input type="text" value="'+( global.profile[global.keys[i]] || ' ')+'" id="'+global.keys[i]+'" name="profileForm"></div></div>';
+              }
              form = form + '</form>';
                         $('#profileview').hide();
                         $('#EditButton').hide();
