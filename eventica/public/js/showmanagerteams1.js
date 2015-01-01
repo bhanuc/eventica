5e85630aa96ab3e253db3f06a6121f96212465b3
bhanuc <bhanuc@iitk.ac.in>
2015-01-01 17:31:35 +0530

    fixing err

--- eventica/public/js/showmanagerteams1.js
+++ eventica/public/js/showmanagerteams1.js
@@ -7,7 +7,7 @@ $.ajax({
         if(data.success && data.teams){
           var str="<thead><tr><th>name</th><th>Created By</th><th>Event</th><th>Submit Status</th><th>Approval Status</th><th>Members</th><th>Approve</th><th>Comment</th><th>Add Comment</th><th>PrintPage</th></tr></thead><tbody>";
           for (var i = data.teams.length - 1; i >= 0; i--) {
-            str = str+ '<tr><td>'+data.teams[i].name+ '</td><td><a class="linked" href="/user/mprofile?id='+data.teams[i].createdby+ '">'+data.teams[i].createdby+'</a></td><td> '+data.teams[i].event+ '</td><td> '+data.teams[i].requestmod+ '</td><td> '+data.teams[i].approved+ '</td><td class="linker" onclick="return myfun(this)"> '+ data.teams[i].members+'</td><td> '+data.teams[i].comments+ '</td><td><form action="/team/managerscomment" type="GET"><input type="text" name="comments"><input type="hidden" name="name" value='+data.teams[i].name+'><button type ="submit">Submit</button></form></td><td> <a href="/team/adminsdissapprove?name='+ data.teams[i].name+'">DisApprove</a></td><td> <a href="/print#'+ data.teams[i].name+'">Print</a></td></tr>';
+            str = str+ '<tr><td>'+data.teams[i].name+ '</td><td><a href="/user/mprofile?id='+data.teams[i].createdby+ '">'+data.teams[i].createdby+'</a></td><td> '+data.teams[i].event+ '</td><td> '+data.teams[i].requestmod+ '</td><td> '+data.teams[i].approved+ '</td><td> '+ data.teams[i].members+'</td><td> '+data.teams[i].comments+ '</td><td><form action="/team/managerscomment" type="GET"><input type="text" name="comments"><input type="hidden" name="name" value='+data.teams[i].name+'><button type ="submit">Submit</button></form></td><td> <a href="/team/adminsdissapprove?name='+ data.teams[i].name+'">DisApprove</a></td><td> <a href="/print#'+ data.teams[i].name+'">Print</a></td></tr>';
           };
           str += '</tbody>'
           $('#teams').html(str);
@@ -17,4 +17,3 @@ $.ajax({
           //success message mybe...
      }
 });
-
