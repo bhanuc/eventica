0d8549c4094b7c4cccf36cb7c81235c851f4f2dc
bhanuc <bhanuc@iitk.ac.in>
2015-01-04 07:09:50 +0530

    updated routes

--- team/team.go
+++ team/team.go
@@ -53,6 +53,7 @@ func Route_setter(ms *mgo.Session, ss *sessions.CookieStore, host, mailfrom, dbn
 	Router.HandleFunc("/team/admincount/{event}", EventCountHandler).Methods("GET")   //show all the teams
 	Router.HandleFunc("/team/admintotalcount", TotalCountHandler).Methods("GET")      //show all the teams
 	Router.HandleFunc("/team/adminsapprove", TeamAdminHandler).Methods("GET")         //show all the teams
+	Router.HandleFunc("/team/managersapprove", TeamManagerHandler).Methods("GET")     //show all the teams
 	Router.HandleFunc("/team/adminsdissapprove", DisTeamAdminHandler).Methods("GET")  //show all the teams
 	Router.HandleFunc("/team/adminscomment", CommentAdminHandler).Methods("GET")      //show all the teams
 	Router.HandleFunc("/team/managerscomment", CommentManagerHandler).Methods("GET")  //show all the teams
