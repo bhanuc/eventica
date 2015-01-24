0a917a10ba6b7bf41e89b7fdb8b86e6c601eb0e8
rnviswanadh291995 <rnviitkian@gmail.com>
2015-01-24 16:31:16 +0530

    changed created by to tech id of team creator

--- eventica/public/js/showadminteams.js
+++ eventica/public/js/showadminteams.js
@@ -92,7 +92,20 @@ $.ajax({
           window.teams =data.teams;
           var str="<thead><tr><th>name</th><th>Created By</th><th>Event</th><th>Submit Status</th><th>Approval Status</th><th>Members</th><th>Approve</th><th>Comment</th><th>Add Comment</th><th>Decline</th><th>PrintPage</th></tr></thead><tbody>";
           for (var i = data.teams.length - 1; i >= 0; i--) {
-            str = str+ '<tr><td>'+data.teams[i].name+ '</td><td><a onclick=showprofile("' + data.teams[i].createdby + '")>' + data.teams[i].createdby + '</a></td><td> '+data.teams[i].event+ '</td><td> '+data.teams[i].requestmod+ '</td><td> '+data.teams[i].approved+ '</td><td> '+ data.teams[i].members+'</td><td> <a href="/team/adminsapprove?name='+ data.teams[i].name+'">Approve</a></td><td> '+data.teams[i].comments+ '</td><td><form action="/team/adminscomment" type="GET"><input type="text" name="comments"><input type="hidden" name="name" value='+data.teams[i].name+'><button type ="submit">Submit</button></form></td><td> <a href="/team/adminsdissapprove?name='+ data.teams[i].name+'">DisApprove</a></td><td> <a href="/print#'+ data.teams[i].name+'">Print</a></td></tr>';
+            var url1;
+            $.ajax({
+              url: '/user/vprofile?id=' + url,
+              type: 'GET',
+              success: function(data) {
+                  console.log(data);
+                  if (data.success) {
+                      var json = data.success;
+                      url1 = json.techid;   
+                  }
+                  //success message mybe...
+              }
+            });
+            str = str+ '<tr><td>'+data.teams[i].name+ '</td><td><a onclick=showprofile("' + data.teams[i].createdby + '")>' +url1+ '</a></td><td> '+data.teams[i].event+ '</td><td> '+data.teams[i].requestmod+ '</td><td> '+data.teams[i].approved+ '</td><td> '+ data.teams[i].members+'</td><td> <a href="/team/adminsapprove?name='+ data.teams[i].name+'">Approve</a></td><td> '+data.teams[i].comments+ '</td><td><form action="/team/adminscomment" type="GET"><input type="text" name="comments"><input type="hidden" name="name" value='+data.teams[i].name+'><button type ="submit">Submit</button></form></td><td> <a href="/team/adminsdissapprove?name='+ data.teams[i].name+'">DisApprove</a></td><td> <a href="/print#'+ data.teams[i].name+'">Print</a></td></tr>';
           };
           str += '</tbody>'
           $('#teams').html(str);
